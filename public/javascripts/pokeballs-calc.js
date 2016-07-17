2dd0f10e180744a3c3a47e6878d42ab94db47a9b
ensley92 <johnensley17@gmail.com>
2016-07-16 23:51:58 -0400

    Typeahead implemented

--- public/javascripts/pokeballs-calc.js
+++ public/javascripts/pokeballs-calc.js
@@ -1,10 +1,37 @@
+var substringMatcher = function(strs) {
+  return function findMatches(q, cb) {
+    var matches, substringRegex;
+
+    // an array that will be populated with substring matches
+    matches = [];
+
+    // regex used to determine if a string contains the substring `q`
+    substrRegex = new RegExp(q, 'i');
+
+    // iterate through the pool of strings and for any string that
+    // contains the substring `q`, add it to the `matches` array
+    $.each(strs, function(i, str) {
+      if (substrRegex.test(str)) {
+        matches.push(str);
+      }
+    });
+
+    cb(matches);
+  };
+};
+
 $(function() {
     $('#js-wildPoke').typeahead({
-        minLength: 3,
-        highlight: true
+        minLength: 1,
+        highlight: true,
+        hint: true
     },
     {
         name: 'pokelist',
-        source: data
+        source: substringMatcher(data)
+    });
+
+    $('#js-wildPoke').bind('typeahead:select', function(e, selection) {
+        console.log('Selection: ' + selection);
     });
 });
